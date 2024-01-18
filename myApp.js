98014d778f90460fa596f4577017e8d392bcde47
Jean-Baptiste Maury <111206519+JbMaury@users.noreply.github.com>
2024-01-18 22:37:40 +0000

    Added conditional response with .env variable

--- myApp.js
+++ myApp.js
@@ -8,4 +8,12 @@ app.get("/", function (req, res) {
   res.sendFile(absolutePath);
 });
 
+app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
+
 module.exports = app;

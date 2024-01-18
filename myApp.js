8132fbeca06efc1691f1dd01be3eefb3b951bef2
Jean-Baptiste Maury <111206519+JbMaury@users.noreply.github.com>
2024-01-18 20:49:10 +0000

    express.static mounted to /public path

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require("express");
 let app = express();
+app.use("/public", express.static(__dirname + "/public"));
 absolutePath = __dirname + "/views/index.html";
 console.log("Hello World");
+
 app.get("/", function (req, res) {
   res.sendFile(absolutePath);
 });

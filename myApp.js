5ecd0a34e265da90699a0eaf0f68dec64053f1fb
Jean-Baptiste Maury <111206519+JbMaury@users.noreply.github.com>
2024-01-18 20:41:57 +0000

    Added app.get for path "/" sending index.html

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
 let express = require("express");
 let app = express();
+absolutePath = __dirname + "/views/index.html";
 console.log("Hello World");
+app.get("/", function (req, res) {
+  res.sendFile(absolutePath);
+});
 
 module.exports = app;
